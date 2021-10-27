fcb05202ca8357d463f4f1fa16b2287769b7e1d2
AshleyJ1994 <34029743+AshleyJ1994@users.noreply.github.com>
2021-10-27 12:52:37 +0100

    commit new

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -30,7 +30,7 @@
         <v-expansion-panel>
           <v-expansion-panel-header class="titleColour">Active Players</v-expansion-panel-header>
           <v-expansion-panel-content>
-            <v-flex v-for="player in players" :key="player.Name">
+            <v-flex>
               <v-card class="card-container rule-cards">
                 <v-card-text class="px-3" :class="{'smaller-display': $vuetify.breakpoint.smAndDown}">
                   <v-card-title>
