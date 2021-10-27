7e3bcd5a21a588442e59a99dcc2e5fcb34c3c8cc
AshleyJ1994 <34029743+AshleyJ1994@users.noreply.github.com>
2021-10-27 12:18:22 +0100

    maimn

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -34,10 +34,10 @@
               <v-card class="card-container rule-cards">
                 <v-card-text class="px-3" :class="{'smaller-display': $vuetify.breakpoint.smAndDown}">
                   <v-card-title>
-                    <h5 class="titleColour">{players.Name}</h5>
+                    <h5 class="titleColour">{{Players.Name}}</h5>
                   </v-card-title>
                   <v-card-subtitle>
-                    <p>{players.Time}</p>
+                    <p>{{Players.Time}}</p>
                   </v-card-subtitle>
                 </v-card-text>
               </v-card>
@@ -100,6 +100,16 @@ export default {
     }
   }
 };
+</script>
+<script>
+      import json from '.\src\assets\json\players.json'
+      export default{
+          data(){
+              return{
+                  Players: json
+              }
+          }
+      }
 </script>
 
 <style lang="scss" scoped>
